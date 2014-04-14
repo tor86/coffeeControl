3af9c6c04a57c7f0c9d6f04a386405e97f790288
Tor Arne Torgersen <tor86@webmail.no>
2014-04-14 18:49:54 +0200

    Added widget update btn

--- coffeeControl/gen/no/hiof/coffeecontrol/R.java
+++ coffeeControl/gen/no/hiof/coffeecontrol/R.java
@@ -25,7 +25,7 @@ public final class R {
         public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090011;
+        public static final int action_settings=0x7f090012;
         public static final int button1=0x7f090001;
         public static final int button2=0x7f090002;
         public static final int button3=0x7f090006;
@@ -40,8 +40,9 @@ public final class R {
         public static final int editText3=0x7f090003;
         public static final int editText4=0x7f090005;
         public static final int imageView1=0x7f090009;
+        public static final int sync_button=0x7f090010;
         public static final int textView1=0x7f09000c;
-        public static final int textView2=0x7f090010;
+        public static final int textView2=0x7f090011;
         public static final int widget_textview=0x7f09000f;
     }
     public static final class layout {
--- coffeeControl/src/no/hiof/coffeecontrol/HelloWidget.java
+++ coffeeControl/src/no/hiof/coffeecontrol/HelloWidget.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 import no.hiof.coffeecontrol.database.DataSource;
 import no.hiof.coffeecontrol.database.SQLiteHelper;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.TextView;
@@ -22,6 +24,9 @@ public class HelloWidget extends AppWidgetProvider {
 
 	private DataSource datasource;
 	//private static SQLiteHelper dbh;
+	
+	// UPDATE: Button code for updating Widget
+	private static final String SYNC_CLICKED    = "automaticWidgetSyncButtonClick";
 	
 	public String getDate() {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd"); 
@@ -44,17 +49,55 @@ public class HelloWidget extends AppWidgetProvider {
 	        
 	        remoteViews = new RemoteViews( context.getPackageName(), R.layout.activity_widget );
 	        watchWidget = new ComponentName( context, HelloWidget.class );
-	        remoteViews.setTextViewText( R.id.widget_textview, amount + " cups of coffee");
+	        //remoteViews.setTextViewText( R.id.widget_textview, amount + " cups of coffee");
+	        remoteViews.setTextViewText( R.id.widget_textview, amount + " cups");
 
+	        // UPDATE: Button code for updating Widget
+	        remoteViews.setOnClickPendingIntent(R.id.sync_button, getPendingSelfIntent(context, SYNC_CLICKED));
+	        
 	        // Updates the widget with a string. Android standard is 30 minutes
 	        // to save battery. We can set custom update frequency by using timer
 	        appWidgetManager.updateAppWidget( watchWidget, remoteViews );
 	    }
 	 
+	 //UPDATE: Button code for updating Widget
+	 @Override
+	    public void onReceive(Context context, Intent intent) {
+	        // TODO Auto-generated method stub
+	        super.onReceive(context, intent);
+
+	        if (SYNC_CLICKED.equals(intent.getAction())) {
+	        	
+	        	// Opens the database and gets amount from current day
+		        datasource = new DataSource(context);
+		        datasource.open();
+		        datasource.updateRow(getDate(), 0);
+		        int amount = datasource.sendToFront();
+		        datasource.close();
+
+	            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+	            RemoteViews remoteViews;
+	            ComponentName watchWidget;
+
+	            remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_widget);
+	            watchWidget = new ComponentName(context, HelloWidget.class);
+
+	            //remoteViews.setTextViewText( R.id.widget_textview, amount + " cups of coffee");
+	            remoteViews.setTextViewText( R.id.widget_textview, amount + " cups");
+	            
+	            //remoteViews.setTextViewText(R.id.sync_button, "TESTING");
+
+	            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+
+	        }
+	    }
 	 
-//	 public void onReceive(Context context, Intent intent) {
-//		 
-//	 }
-	 
+	 // UPDATE: Button code for updating Widget
+	 protected PendingIntent getPendingSelfIntent(Context context, String action) {
+	        Intent intent = new Intent(context, getClass());
+	        intent.setAction(action);
+	        return PendingIntent.getBroadcast(context, 0, intent, 0);
+	    }
 	 
 }
